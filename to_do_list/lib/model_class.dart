0687582c40a284a457ef16e95fca03042c7edb71
GaneshRawool18 <ganeshrawool07@gmail.com>
2025-01-03 23:45:45 +0530

    added model class file

---
+++ to_do_list/lib/model_class.dart
@@ -0,0 +1,11 @@
+class ShowModelClass {
+  String title ;
+  String description;
+  String date;
+
+  ShowModelClass({
+   required this.title,
+   required this.description,
+   required this.date
+  });
+}
